d8b01aa1abe455d42b3146a12743f5307e996807
kay <kaych26@gmail.com>
2020-07-01 23:16:12 -0400

    added pagination and buttons

--- src/main.js
+++ src/main.js
@@ -1,50 +1,91 @@
+// const { start } = require("live-server");
 
-
-const KEY = "17293224-4fce193fa3ddcc5b64747d858";
+const KEY = '17293224-4fce193fa3ddcc5b64747d858';
 const query = `https://pixabay.com/api/?key=${KEY}&image_type=photo&per_page=60&q=`;
 
 const form = document.querySelector('.form-input');
 const input = document.querySelector('#user-input');
 const returnMsg = document.querySelector('#return-msg');
+const pageWrapper = document.querySelector('.page-button');
 const imageContainer = document.querySelector('.image-container');
 
 
-const displayResults = data => {
-  imageContainer.innerHTML = '';
+let current_page = 3;
+let image_per_page = 10;
+let images= '';
+
+const setUpPages = total_img => {
+
+  pageWrapper.innerHTML = '';
+
+  let pages = Math.ceil(total_img / image_per_page);
+  // debugger;
+  
+  for (let i = 1; i < pages + 1; i++) {
+    let button = pageButtons(i);
+    pageWrapper.appendChild(button);
+  }
+  // for (let page = 1; page <= pages; page++) {
+  //   pageWrapper.innerHTML += `<button value=${page} class="page">${page}</button>`;
+  // }
+};
+
+const pageButtons = (page) => {
+  
+  let button = document.createElement('button');
+  button.innerText = page;
+  
+  if (current_page == page)
+    button.classList.add('active');
+  
+  button.addEventListener('click', function () {
+    current_page = page;
+    displayResults(current_page)
+  });
+  
+  return button;
+  
+};
+
+
+const displayResults = (page_idx) => {
   let html = '';
   let img = '';
-  
-  for (let i = 0; i < data.length; i++) {
-    img = `<img src="${data[i].previewURL}" alt="image"></img>`
+  imageContainer.innerHTML = '';
+  page_idx -= 1;
+
+  let start = image_per_page * page_idx;
+  let end = start + image_per_page;
+  for (let i = start; i < end; i++) {
+
+    // let img = results[i].previewURL;
+    // let img_element = document.createElement('')
+
+    img = `<img src="${images[i].previewURL}" alt="image"></img>`;
     html += `<div>${img}</div>`;
   }
-  if (html)
+  if (html) {
     imageContainer.innerHTML = html;
-  else
-    returnMsg.innerHTML = "<p>sorry, no images available...</p>";
-}
+    setUpPages(images.length);
+  }
+  else returnMsg.innerHTML = '<p>sorry, no images available...</p>';
+};
 
-form.addEventListener('submit', async event => {
+
+form.addEventListener('submit', async (event) => {
   event.preventDefault();
   returnMsg.innerHTML = '';
 
   if (input.value) {
     const endpoint = query + input.value;
     let response = await axios.get(endpoint);
-    // debugger;
-    if (response) {
-      displayResults(response.data.hits)
+    images = response.data.hits;
+   
+    // debugger
+    if (images) {
+      displayResults(current_page);
     }
-    
-  }
-  else {
+  } else {
     returnMsg.innerHTML = '<p>Enter a theme ...</p>';
   }
-
 });
-
-
-
-
-
-
