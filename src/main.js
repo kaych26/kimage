e34844d0cc0b6ee2ccaacf2e8e0c86ee7a6f084c
kay <kaych26@gmail.com>
2020-07-03 14:26:39 -0400

    refactor code

--- src/main.js
+++ src/main.js
@@ -14,132 +14,132 @@ const input = document.querySelector('#user-input');
 const returnMsg = document.querySelector('#return-msg');
 const pageWrapper = document.querySelector('.page-button');
 const imgThumbnail = document.querySelector('.img-thumbnail');
-const select= document.querySelector('.select');
+const select = document.querySelector('.select');
 const modalEle = document.querySelector('.modal');
 const modalImage = document.querySelector('.modalImage');
 const popularEle = document.querySelector('.popular');
 
-const image_per_page = 10; // # of pages per display
+const image_per_page = 10;
 let current_page = 1;
-let images = '';
-const popularArr = ['nature', 'food', 'beach', 'flower', 'animal', 'car'];
 
 /*----------------------------------------------------------/
-  setUpPages => calculate the buttons needed for pagination.
+setUpPages => calculate the buttons needed for pagination.
 -----------------------------------------------------------*/
 
-const setUpPages = (total_img) => {
+const setUpPages = (images) => {
   pageWrapper.innerHTML = '';
-  
-  let pages = Math.ceil(total_img / image_per_page);
-  
-  for (let i = 1; i < pages + 1; i++) {
-    let button = pageButtons(i);
+  let pages = Math.ceil(images.length / image_per_page);
+
+  for (let page = 1; page <= pages; page++) {
+    let button = document.createElement('button');
+    button.innerText = page;
+
+    if (current_page == page) button.classList.add('active');
+
+    button.addEventListener('click', (event) => {
+      event.preventDefault();
+      let prevPage = document.querySelector('.page-button button.active');
+      prevPage.classList.remove('active');
+
+      current_page = button.innerText;
+      button.classList.add('active');
+
+      displayResults(current_page, images);
+    });
+
     pageWrapper.appendChild(button);
   }
 };
 
-/*----------------------------------------------------------/
-  pageButtons => create buttons for pagination.
------------------------------------------------------------*/
-const pageButtons = (page) => {
-  let button = document.createElement('button');
-  button.innerText = page;
-  
-  if (current_page == page) button.classList.add('active');
-  
-  button.addEventListener('click', ()=>{
-    current_page = page;
-    displayResults(current_page);
-  });
-  
-  return button;
-};
-
 /* ----------------------------------------------------------/
-  displayResults => display the thumbnails and setting modal 
-  using the large image.
+displayResults => display the thumbnails and setting modal 
+using the large image.
 ------------------------------------------------------------*/
-
-const displayResults = (page_idx) => {
+const displayResults = (page_idx, images) => {
+  returnMsg.innerHTML = '';
   imgThumbnail.innerHTML = '';
   page_idx -= 1;
-  
+
   let start = image_per_page * page_idx;
   let end = start + image_per_page;
-  
+
   for (let i = start; i < end; i++) {
     let elem = document.createElement('img');
-    
-    elem.setAttribute("src", images[i].previewURL);
+
+    elem.setAttribute('src', images[i].previewURL);
     elem.setAttribute('alt', 'image');
     imgThumbnail.appendChild(elem);
-    
-    // calculate and create buttons for pagination
-    setUpPages(images.length);
-    
+
     // display modal using the large image
-    elem.addEventListener("click", event => {
+    elem.addEventListener('click', (event) => {
       event.preventDefault();
-      modalEle.style.display = "block";
+      modalEle.style.display = 'block';
       modalImage.src = images[i].largeImageURL;
     });
-    
+
     // close modal
-    document.querySelector(".close").addEventListener("click", () => {
-      modalEle.style.display = "none";
+    document.querySelector('.close').addEventListener('click', () => {
+      modalEle.style.display = 'none';
     });
   }
 };
 
 /* ----------------------------------------------------------/
-  popularSearch => To allow user to click on the popular 
-  key words to generate the image gallery
+  getImage => axios call to retrieve images
 ------------------------------------------------------------*/
+const getImages = async (word) => {
+  const endpoint = query + '&per_page=' + select.value + '&q=' + word;
+  let response = await axios.get(endpoint);
 
-const popularSearch = () => {
+  const images = response.data.hits;
+
+  if (images.length > 0) {
+    displayResults(current_page, images);
+    setUpPages(images);
+  } else returnMsg.innerHTML = 'No image available.';
+};
+
+/* ----------------------------------------------------------/
+  popularSearch => Set up popular word list and generate the
+  image result when user clicks it.
+------------------------------------------------------------*/
+const popularImages = () => {
+  const popularArr = ['nature', 'food', 'beach', 'flower', 'animal', 'car'];
   for (let i = 0; i < popularArr.length; i++) {
     let elem = document.createElement('a');
     elem.setAttribute('href', '#');
     elem.innerText = `${popularArr[i]} `;
     popularEle.appendChild(elem);
-    
+
     // user clicked the popular key word
     elem.addEventListener('click', async (event) => {
       event.preventDefault();
-      const endpoint = query + "&per_page="+ select.value + "&q="+ popularArr[i];
-      let response = await axios.get(endpoint);
-      
-      images = response.data.hits;
-      
-      if (images) 
-        displayResults(current_page);
+      getImages(popularArr[i]);
     });
   }
+};
+
+/*----------------------------------------------------------/
+  handleUserInput => user input
+-----------------------------------------------------------*/
+const handleUserInput = () => {
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+  
+    // user input
+    if (input.value) getImages(input.value);
+    // blank user input
+    else returnMsg.innerHTML = 'Enter a search or select popular list.';
+  });
 }
 
 /*----------------------------------------------------------/
   Main
 -----------------------------------------------------------*/
 
-// Populate the popular key words for user to click
-popularSearch();
+// Set up the popular key words for user to click
+popularImages();
 
-// Form event to retrieve image based on user input
-form.addEventListener('submit', async (event) => {
-  event.preventDefault();
-  returnMsg.innerHTML = '';
-  
-  if (input.value) {
-    const endpoint = query + "&per_page="+ select.value + "&q="+ input.value;
-    let response = await axios.get(endpoint);
-    
-    images = response.data.hits;
-    
-    if (images) 
-      displayResults(current_page);
-  
-  } else {
-    returnMsg.innerHTML = 'No image ...';
-  }
-});
+// user input field
+handleUserInput();
